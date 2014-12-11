4730b4a24c5a522d48b996026d5e3be2be4b8949
Benjamin Reinhart <ben@Benjamins-MacBook-Air.local>
2014-12-10 19:32:01 -0600

    Added relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :accounts
+  has_many :transactions, through: :accounts
   has_secure_password
 
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211011949) do
+ActiveRecord::Schema.define(version: 20141211012546) do
 
   create_table "accounts", force: true do |t|
     t.integer  "user_id"
     t.decimal  "balance"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "transactions", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "account_id"
+    t.decimal  "amount"
+    t.boolean  "withdraw"
+    t.boolean  "deposit"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
